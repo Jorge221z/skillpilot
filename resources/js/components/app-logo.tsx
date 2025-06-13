5eefe897f126efda1ad135ad08d4bb56caa12229
Jorge221z <7460062@alu.murciaeduca.es>
2025-06-13 14:01:45 +0200

    New header logo designç

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -7,8 +7,11 @@ export default function AppLogo() {
                 <AppLogoIcon className="size-12" />
             </div>
             <div className="-ml-5.5 grid flex-1 text-left">
-                <span className="mb-0.5 truncate leading-tight font-bold text-xl">SkillPilot</span>
-            </div>
+  <span className="mb-0.5 truncate leading-tight font-bold text-xl">
+    <span className="text-blue-400">Skill</span>
+    <span className="text-orange-500">Pilot</span>
+  </span>
+</div>
         </>
     );
 }
