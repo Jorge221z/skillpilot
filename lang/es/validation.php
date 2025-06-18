3fc27a8fecd10194dfe3318bcdb5010b840fba86
Jorge221z <7460062@alu.murciaeduca.es>
2025-06-19 00:27:49 +0200

    Add new validation keys for spanish language

--- lang/es/validation.php
+++ lang/es/validation.php
@@ -2,8 +2,10 @@
 
 return [
     'confirmed' => 'Las contraseñas no coinciden.',
+    'current_password' => 'La contraseña actual es incorrecta.',
     'min' => [
         'string' => 'La contraseña debe contener al menos 8 caracteres.',
     ],
+    'required' => 'Este campo es obligatorio.',
     'unique' => 'El correo electronico ya ha sido registrado.',
 ];
